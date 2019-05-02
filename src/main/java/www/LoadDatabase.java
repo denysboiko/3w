9e28da8e3b7a8764d210b092bd9ba5f9d249dfd8
Denys Boiko <denys.boiko@gmail.com>
2019-05-03 00:29:44 +0300

    add database loader

---
+++ src/main/java/www/LoadDatabase.java
@@ -0,0 +1,19 @@
+package www;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class LoadDatabase {
+    @Bean
+    CommandLineRunner initDatabase(UserRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new User("Bilbo Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new User("Frodo Baggins", "thief")));
+        };
+    }
+}
